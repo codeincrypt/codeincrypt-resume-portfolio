24d95a955aa9efb06796538b28dcdf65426bb4f8
codeincrypt <kartikkr555@gmail.com>
2023-06-17 00:14:47 +0530

    resume setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import data from "./resume.json";
 
 function App() {
-  
   const [cursorStyle, setCursorStyle] = React.useState({});
 
   const handleMouseMove = (e) => {
@@ -22,7 +21,7 @@ function App() {
 
   return (
     <div className="App" onMouseMove={handleMouseMove}>
-        <div className="cursor" style={cursorStyle}></div>
+      {/* <div className="cursor" style={cursorStyle}></div> */}
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-4">
@@ -72,13 +71,15 @@ function App() {
 
           <div className="col-lg-1"></div>
           <div className="col-lg-5 rightsidebar">
-            <div className="summary ">
+            <div id="summary" className="summary ">
+              <h4 className="title">Summary</h4>
               {data.about.map((item, index) => (
-                <p>{item}</p>
+                <p className="card-text4 mb-0">{item}</p>
               ))}
             </div>
 
             <div id="work-experience" className="work-experience margintop">
+              <h4 className="title">Work Experience</h4>
               {data.workExperience.map((item, index) => (
                 <div className="main-card">
                   <div className="row">
@@ -107,12 +108,44 @@ function App() {
             </div>
 
             <div id="education" className="education margintop">
-            <h4 className="title">EDUCATION</h4>
+              <h4 className="title">PROJECTS</h4>
+              {data.projects.map((item, index) => (
+                <div className="main-card">
+                  <h4 className="card-text1">{item.name}</h4>
+                  <ul className="lists">
+                    {item.summary.map((item, index) => (
+                      <li>{item}</li>
+                    ))}
+                  </ul>
+
+                  <div>
+                    {item.technology.map((item, index) => (
+                      <button className="chips">{item}</button>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div id="education" className="education margintop">
+              <h4 className="title">EDUCATION</h4>
               {data.education.map((item, index) => (
                 <div className="main-card">
                   <h4 className="card-text1">{item.degree}</h4>
                   <h5 className="card-text3">{item.college}</h5>
-                  <p className="card-text2">{item.year}</p>
+                  <p className="card-text2 mb-0">{item.year}</p>
+                </div>
+              ))}
+            </div>
+
+            <div id="skills" className="skills margintop">
+              <h4 className="title">skills</h4>
+              {data.skills.map((item, index) => (
+                <div className="main-card">
+                  <h6 className="mb-0">
+                    <b className="card-text5">{item.name}</b> :{" "}
+                    <span className="card-text4">{item.details}</span>
+                  </h6>
                 </div>
               ))}
             </div>
@@ -122,7 +155,7 @@ function App() {
               {data.language.map((item, index) => (
                 <div className="main-card">
                   <h4 className="card-text1">{item.name}</h4>
-                  <h5 className="card-text3 text-right  ">{item.level}</h5>
+                  <h5 className="card-text2 text-right mb-0">{item.level}</h5>
                 </div>
               ))}
             </div>
